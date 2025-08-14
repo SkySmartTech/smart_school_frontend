dcdbd1c03f3579001f6d42db547d4a6026f4e883
SewHewage <tsewmini.hewage@gmail.com>
2025-08-14 17:35:24 +0530

    Route code enhancement

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -85,7 +85,6 @@ function AppRoutes() {
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/addmarks" element={<AddMarks />} />
 
-        {/* <Route path="/studentdashboard" element={<StudentDashboard />} /> */}
         <Route path="/teacherdashboard" element={<TeacherDashboard />} />
         <Route path="/commondashboard" element={<CommonDashboard />} />
         <Route path="/systemManagement" element={<SystemManagementPage />} />
@@ -103,7 +102,7 @@ function AppRoutes() {
         <Route path="/userAccessManagement" element={<UserAccessManagement/>} /> */}
 
 
-        {/* <Route path="/dayPlan" element={<DayPlanUpload />} />
+        {/* 
         <Route path="/studentdashboard" element={<StudentDashboard />} />
         <Route path="/production" element={<ProductionUpdatePage />} />
         */}
@@ -111,11 +110,10 @@ function AppRoutes() {
         <Route path="/teacherReport" element={<ClassTeacherReport/>} />
         <Route path="/parentReport" element={<ParentReport/>} />
         
-        {/* <Route path="/help" element={<HelpPage />} />
+        {/* 
         <Route path="/setting" element={<SettingPage />} />
         <Route path="/dayReport" element={<DayPlanReport />} />
         <Route path="/daySummary" element={<DayPlanSummary />} />
-        <Route path="/userManagement" element={<UserManagement />} />
          */}
         
 
